3c6d54981d11b7f5e7cc5eec17423f41dfb5d272
mert.alptekin <mert.alptekin@neomina.com>
2023-05-05 12:56:48 +0300

    EF Core Packages

---
+++ InterfacesApp/Models/EFRepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesApp.Models
+{
+  // TContext farklı DbContextler ile çalışmamızı sağlayacak
+  public abstract class EFRepositoryBase<TContext>
+  {
+    protected TContext context;
+   
+
+    public EFRepositoryBase(TContext context)
+    {
+      this.context = context;
+    }
+
+
+    public virtual int SaveChanges()
+    {
+      return 1;
+    }
+
+  }
+}
